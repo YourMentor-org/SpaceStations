a4fb2701ba4a56f186842d9cfc3f4cf7654d94e9
Static15 <staticrock@mail.ru>
2017-05-23 00:07:04 +0500

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,42 @@ document.getElementById("bar").classList.toggle("bar-rot");
 }
 
 burger.addEventListener("click", showMenu, false);
+
+
+// function for slider
+
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function currentDiv(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("slider__button");
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slider__button_active", "");
+  }
+
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slider__button_active";
+}
+
+var paginationItem =[].slice.call(document.getElementsByClassName("slider__button"));
+
+paginationItem.forEach(function (elem, i){
+  elem.addEventListener("click", function(){
+    currentDiv(i+1)
+  });
+});

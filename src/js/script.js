8172c62728969208bce0427389aca51b56abb621
Static15 <staticrock@mail.ru>
2017-06-03 22:51:56 +0500

    add slider for contacts, fixed menu birger

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,14 @@
-var burger = document.getElementById("header__nav"); //обращаемся по id 
+// var burger = document.getElementById("header__nav"); //обращаемся по id 
 
-function showMenu(e) {
-  e.preventDefault();
-document.body.classList.toggle("open");
-burger.classList.toggle("menu-expanded");
-document.getElementById("nav-list").classList.toggle("show");
-document.getElementById("bar").classList.toggle("bar-rot");
-  setTimeout(showMenu, 2000);
-}
+// function showMenu(e) {
+//   e.preventDefault();
+// document.body.classList.toggle("open");
+// burger.classList.toggle("menu-expanded");
+// document.getElementById("nav-list").classList.toggle("show");
+// document.getElementById("bar").classList.toggle("bar-rot");
+// }
 
-burger.addEventListener("click", showMenu, false);
+// burger.addEventListener("click", showMenu, false);
 
 
 // function for slider
@@ -49,3 +48,114 @@ paginationItem.forEach(function (elem, i){
     currentDiv(i+1)
   });
 });
+
+//new burger menu more responsive
+
+var theToggle = document.getElementById('toggle');
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+var theToggle = document.getElementById('toggle');
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+      elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
+
+
+
+//slider for contacts
+
+var controls = document.querySelectorAll('.controls');
+for(var i=0; i<controls.length; i++){
+  controls[i].style.display = 'inline-block';
+}
+
+var slides = document.querySelectorAll('#catalog .catalog__item');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+  goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+  goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+  slides[currentSlide].className = 'catalog__item';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'catalog__item showing';
+}
+
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+function pauseSlideshow(){
+  pauseButton.innerHTML = '&#9658;'; // play character
+  playing = false;
+  clearInterval(slideInterval);
+}
+
+function playSlideshow(){
+  pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
+  playing = true;
+  slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function(){
+  if(playing){ pauseSlideshow(); }
+  else{ playSlideshow(); }
+};
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+next.onclick = function(){
+  pauseSlideshow();
+  nextSlide();
+};
+previous.onclick = function(){
+  pauseSlideshow();
+  previousSlide();
+};

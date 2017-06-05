96275563681aa3d81cf4f5307498c72946aaa3fd
Static15 <staticrock@mail.ru>
2017-06-05 22:58:48 +0500

    add imagemin,svgmin task, create task build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var concat = require('gulp-concat');
 var clean = require('gulp-clean');
 var cssnano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var svg2png = require('gulp-svg2png');
+var svgmin = require('gulp-svgmin');
 
 gulp.task('styles', function() {
 	return gulp.src('/*.css')
@@ -18,9 +21,31 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('out'));
 });
 
+
+gulp.task('svgmin', function () {
+    return gulp.src('logo.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('out/images'))
+);
+
+gulp.task('svg2png', function () {
+    gulp.src('./img/*.svg')
+        .pipe(svg2png())
+        .pipe(gulp.dest('./out'));
+});
+
 gulp.task('default', ['clean'], function() {  
     gulp.run('scripts');
     gulp.run('styles');
+    gulp.run('scripts');
+    gulp.run('imagemin');
+    gulp.run('watch');
 });
 
 gulp.task('scripts', function () {
@@ -38,3 +63,5 @@ gulp.task('clean', function() {
   return gulp.src('out', {read: false})
    .pipe(clean());
 });
+
+gulp.task('build', ['clean', 'styles','scripts', 'imagemin']);

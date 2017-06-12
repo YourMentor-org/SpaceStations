4cf7c679f1e02dec270b59955b60bebf4a743d70
static15 <staticrock@mail.ru>
2017-06-13 00:15:57 +0500

    add build task and default

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var autoprefixer = require('gulp-autoprefixer');
 var concatCss = require('gulp-concat-css');
 var cssnano = require('gulp-cssnano');
 var cleanCSS = require('gulp-clean-css');
+var svgmin = require('gulp-svgmin');
+var imagemin = require('gulp-imagemin');
+var svg2png = require('gulp-svg2png');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function() { // Создаем таск "sass"
   return gulp.src('./src/scss/*.scss') // Берем источник
@@ -31,3 +35,37 @@ gulp.task('clean', function() {
 gulp.task('watch', function() {
 	gulp.watch(['src/js/*.js', 'src/styles/*.css', '*.html'])
 });
+
+gulp.task('svgmin', function () {
+    return gulp.src('./src/img/*.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('out/images'))
+);
+
+gulp.task('svg2png', function () {
+    gulp.src('./src/img/*.svg')
+        .pipe(svg2png())
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('default', ['clean'], function() {  
+    gulp.run('scripts');
+    gulp.run('styles');
+    gulp.run('scripts');
+    gulp.run('watch');
+});
+
+gulp.task('scripts', function () {
+	gulp.src('src/js/*.js')
+	.pipe(concat('all.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('out'));	
+});
+
+gulp.task('build', ['clean', 'styles','scripts', 'imagemin','svgmin','svg2png']);
